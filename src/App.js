966a77f08c3f41b83684e1daff6c6c91b9f92a78
“ourobin2” <ozxing99@email.com>
2018-06-28 21:05:46 +0800

    add article

--- src/App.js
+++ src/App.js
@@ -1,19 +1,68 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
+import './article.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="App ">
+        <div class='block'>
+           <div class='row'>
+            <div class='col-md-4'>
+              <div class='font'>我的文章</div>
+            <button type="button" class="btn btn-primary">add</button>
+          </div>
+        </div>
+
+        <div class='content'>
+          <img src="https://www.taiwan.net.tw/resources/images/Attractions/0001095.jpg" alt="image" width="386.43"
+    height="233.82" class="image">
+          </img>
+            <div>
+              <div class='font-title'>風景照
+              </div>
+              <button type="button" class="btn btn-success">edit</button>
+              <button type="button" class="btn btn-danger">delete</button>
+            </div>
+          <div class='font-content'>很美的山!</div>
+          <button type="button" class="btn btn-link">...more</button>
+        </div>
+
+        <div class='content'>
+          <img src="https://www.taiwan.net.tw/resources/images/Attractions/0001095.jpg" alt="image" width="386.43"
+    height="233.82" class="image">
+          </img>
+            <div>
+              <div class='font-title'>風景照
+              </div>
+              <button type="button" class="btn btn-success">edit</button>
+              <button type="button" class="btn btn-danger">delete</button>
+            </div>
+          <div class='font-content'>很美的山!</div>
+          <button type="button" class="btn btn-link">...more</button>
+        </div>
+
+        <div class='content'>
+          <img src="https://www.taiwan.net.tw/resources/images/Attractions/0001095.jpg" alt="image" width="386.43"
+    height="233.82" class="image">
+          </img>
+            <div>
+              <div class='font-title'>風景照
+              </div>
+              <button type="button" class="btn btn-success">edit</button>
+              <button type="button" class="btn btn-danger">delete</button>
+            </div>
+          <div class='font-content'>很美的山!</div>
+          <button type="button" class="btn btn-link">...more</button>
+        </div>
+
+        
+
+
+
+
       </div>
+    </div>
     );
   }
 }
